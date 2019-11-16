75c5ab18eeeb6a388c7af7cf59813797569c1618
¨Cabada¨ <j_cabada@hotmail.com>
2019-11-15 23:29:49 -0700

    Se agregaron las dependencias a GIT.

---
+++ PaginaWeb/componentes/TablaRegistroCurso.php
@@ -0,0 +1,43 @@
+
+<div class="row">
+    <div class="col-sm-12">
+        <h2>REGISTRO DE CURSOS DE FORMACIÓN DOCENTE Y ACTUALIZACIÓN PROFESIONAL</h2>
+        <caption>
+            <button class="btn btn-primary" data-toggle="modal" data-target="#modalNuevo">
+                Agregar nuevo
+                <span class="fas fa-plus"></span>
+            </button>
+        </caption>
+        <table class="table table-hover table-condensed table-bordered table-striped white-background">
+
+
+            <tr>
+                <td>Nombre de curso</td>
+                <td>Periodo</td>
+                <td>Horas de capacitacion</td>
+                <td>Numero de participantes Base</td>
+                <td>Numero de participantes Honorarios/Itinerarios</td>
+                <td>Total</td>
+                <td>Editar</td>
+                <td>Eliminar</td>
+            </tr>
+
+            <tr>
+                <td>Moodle como herramienta para el desempeño del docente.</td>
+                <td>Ene 2018</td>
+                <td>30</td>
+                <td>9</td>
+                <td>9</td>
+                <td>18</td>
+                <td>
+                    <button class="btn btn-warning fas fa-pencil-alt" data-toggle="modal" data-target="#modalEdicion"></button>
+                </td>
+                <td>
+                    <button class="btn btn-danger <i fas fa-window-close "></button>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+</div>
+
